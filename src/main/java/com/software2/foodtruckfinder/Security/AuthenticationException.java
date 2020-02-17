2072a10bcd407d58258a7b3e8f7347da83084cce
Connor-woodahl <Connor_woodahl1@baylor.edu>
2020-02-17 14:23:27 -0600

    This might be it

--- src/main/java/com/software2/foodtruckfinder/Security/AuthenticationException.java
+++
@@ -1,7 +0,0 @@
-package com.software2.foodtruckfinder.Security;
-
-public class AuthenticationException extends RuntimeException {
-    public AuthenticationException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
