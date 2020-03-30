72dcb43807475379c7024595c15e2c12e024ed25
MarkFuller1 <MarkFuller1@baylor.edu>
2020-03-30 16:49:54 -0700

    emails

---
+++ src/main/java/com/software2/foodtruckfinder/secure/service/Email.java
@@ -0,0 +1,46 @@
+package com.software2.foodtruckfinder.secure.service;
+// File Name SendEmail.java
+
+import java.util.*;
+import javax.mail.*;
+import javax.mail.internet.*;
+
+public class Email {
+
+    public boolean sendEmail(String from, String to, String text, String subject) {
+        String host = "localhsost";
+
+        Properties properties = System.getProperties();
+
+        // Setup mail server
+        properties.setProperty("mail.smtp.host", host);
+
+        // Get the default Session object.
+        Session session = Session.getDefaultInstance(properties);
+
+        try {
+            // Create a default MimeMessage object.
+            MimeMessage message = new MimeMessage(session);
+
+            // Set From: header field of the header.
+            message.setFrom(new InternetAddress(from));
+
+            // Set To: header field of the header.
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+
+            // Set Subject: header field
+            message.setSubject(subject);
+
+            // Now set the actual message
+            message.setText(text);
+
+            // Send message
+            Transport.send(message);
+            System.out.println("Sent message successfully " + to + " <- " + from);
+        } catch (MessagingException mex) {
+            mex.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+}
