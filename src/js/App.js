dde07c8e08196a9ce653d3c7919084cfe774a4fe
Nathan Hill <nathanhill1110@gmail.com>
2020-04-11 13:10:13 -0500

    Theme Added

--- src/js/App.js
+++ src/js/App.js
@@ -14,25 +14,28 @@ const baylorTheme = createMuiTheme({
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <HashRouter>
-          <div>
-            <Route exact path="/" component={Pages.Home} />
-            <Route exact path="/create_account" component={Pages.CreateAccount} />
-            <Route exact path="/Request" component={Pages.DatabaseListing} />
-            <Route exact path="/loginpage" component={Pages.Login} />
-            <Route exact path="/Notifications" component={Pages.Notifications} />
-            <Route exact path="/SendNotifications" component={Pages.SendNotification} />
-            <Route exact path="/Profile" component={Pages.UserProfile} />
-            <Route exact path="/TestRouting" component={Pages.TestRouting} />
-            <Route exact path="/FoodTruckTable" component={Pages.Table} />
-            <Route exact path="/FoodTruckDetails" component={Pages.TruckDetails} />
-            <Route exact path="/UserProfile" component={Pages.UserProfile} />
-            {/* <Route exact path="/OwnerProfile" component={Pages.OwnerProfile} /> */}
-            <Route exact path="/FoodTruckPreference" component={Pages.CustPreferences} />
-          </div>
-        </HashRouter>
-      </div>
+      <ThemeProvider theme={baylorTheme}>
+        <div>
+          <HashRouter>
+            <div>
+              <Route exact path="/" component={Pages.Home} />
+              <Route exact path="/create_account" component={Pages.CreateAccount} />
+              <Route exact path="/Request" component={Pages.DatabaseListing} />
+              <Route exact path="/loginpage" component={Pages.Login} />
+              <Route exact path="/Notifications" component={Pages.Notifications} />
+              <Route exact path="/SendNotifications" component={Pages.SendNotification} />
+              <Route exact path="/Profile" component={Pages.UserProfile} />
+              <Route exact path="/TestRouting" component={Pages.TestRouting} />
+              <Route exact path="/FoodTruckTable" component={Pages.Table} />
+              <Route exact path="/FoodTruckDetails" component={Pages.TruckDetails} />
+              <Route exact path="/UserProfile" component={Pages.UserProfile} />
+              {/* <Route exact path="/OwnerProfile" component={Pages.OwnerProfile} /> */}
+              <Route exact path="/FoodTruckPreference" component={Pages.CustPreferences} />
+            </div>
+          </HashRouter>
+        </div>
+        
+        </ThemeProvider>
     );
   }
 }
