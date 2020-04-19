7cfbe48f59d23b4f5519c4d976550edcfb61c0a7
Nathan Hill <nathanhill1110@gmail.com>
2020-04-19 13:48:30 -0500

    Added map button for latitude and longitude

--- src/js/components/finalTruckTable.js
+++ src/js/components/finalTruckTable.js
@@ -5,20 +5,20 @@ import FormControl from "@material-ui/core/FormControl";
 import NativeSelect from "@material-ui/core/NativeSelect";
 import OutlinedInput from "@material-ui/core/OutlinedInput";
 import InputAdornment from "@material-ui/core/InputAdornment";
-
+import ScheduleMap from './scheduleMap'
 import { connect } from "react-redux";
 import * as Request from "./../helpers/backendRequests";
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import InputBase from "@material-ui/core/InputBase";
 import TextField from "@material-ui/core/TextField";
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
 import { ExpansionPanel, ExpansionPanelDetails, ExpansionPanelSummary, Typography } from "@material-ui/core";
 
 const BootstrapInput = withStyles((theme) => ({
-  root: {
-    "label + &": {
-      marginTop: theme.spacing(3),
-    },
-  },
   textField: {
     marginLeft: theme.spacing(1),
     marginRight: theme.spacing(1),
@@ -27,32 +27,10 @@ const BootstrapInput = withStyles((theme) => ({
   margin: {
     margin: theme.spacing(1),
   },
-  input: {
-    borderRadius: 4,
-    position: "relative",
-    backgroundColor: theme.palette.background.paper,
-    border: "1px solid #ced4da",
-    fontSize: 16,
-    padding: "10px 26px 10px 12px",
-    transition: theme.transitions.create(["border-color", "box-shadow"]),
-    // Use the system font instead of the default Roboto font.
-    fontFamily: [
-      "-apple-system",
-      "BlinkMacSystemFont",
-      '"Segoe UI"',
-      "Roboto",
-      '"Helvetica Neue"',
-      "Arial",
-      "sans-serif",
-      '"Apple Color Emoji"',
-      '"Segoe UI Emoji"',
-      '"Segoe UI Symbol"',
-    ].join(","),
-    "&:focus": {
-      borderRadius: 4,
-      borderColor: "#80bdff",
-      boxShadow: "0 0 0 0.2rem rgba(0,123,255,.25)",
-    },
+ 
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
   },
 }))(InputBase);
 
@@ -60,11 +38,14 @@ class FinalTruckTable extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      open: false,
       ownerTruckID: props.auth.user.sub,
       data: [],
       schedule: [],
     };
     this.onSubmit = this.onSaveRow.bind(this);
+    this.handleClickOpen = this.handleClickOpen.bind(this);
+    this.handleClose = this.handleClose.bind(this);
   }
 
   async componentDidMount() {
@@ -219,6 +200,13 @@ class FinalTruckTable extends React.Component {
     Request.updateTruckByID(tdata); 
   }
 
+  handleClickOpen = () => {
+    this.setState({open: true});
+  };
+
+  handleClose = () => {
+    this.setState({open: false});
+  };
   
 
   render() {
@@ -319,53 +307,28 @@ class FinalTruckTable extends React.Component {
                                     />
                                   </td>
                                   <td>
-                                    Location
-                                    <td>
-                                      <FormControl
-                                        fullWidth
-                                        className={classes.margin}
-                                        variant="outlined"
-                                      >
-                                        <InputLabel htmlFor="outlined-adornment-amount">
-                                          Latitude
-                                        </InputLabel>
-                                        <OutlinedInput
-                                          id="outlined-adornment-amount"
-                                          value={this.state.schedule[idx].monLatitude}
-                                          onChange={this.handleScheduleChange(idx)}
-                                          startAdornment={
-                                            <InputAdornment position="start">
-                                              lat:
-                                            </InputAdornment>
-                                          }
-                                          labelWidth={30}
-                                        />
-                                      </FormControl>
-                                    </td>
-                                    <td>
-                                      <FormControl
-                                        fullWidth
-                                        className={classes.margin}
-                                        variant="outlined"
-                                      >
-                                        <InputLabel htmlFor="outlined-adornment-amount">
-                                          Longitude
-                                        </InputLabel>
-                                        <OutlinedInput
-                                          id="outlined-adornment-amount"
-                                          value={
-                                            this.state.schedule[idx].monLongitude
-                                          }
-                                          onChange={this.handleScheduleChange(idx)}
-                                          startAdornment={
-                                            <InputAdornment position="start">
-                                              Lon:
-                                            </InputAdornment>
-                                          }
-                                          labelWidth={30}
-                                        />
-                                      </FormControl>
-                                    </td>
+                                  <Button onClick={this.handleClickOpen}>Set Truck Location</Button>
+                                    <Dialog disableBackdropClick disableEscapeKeyDown open={this.state.open} onClose={this.handleClose}
+                                    maxWidth={"lg"}
+                                    fullWidth="true">
+                                      <DialogTitle>Drop a Pin Where Your Truck Is</DialogTitle>
+                                      <DialogContent fullWidth="true">
+                                        <form className={BootstrapInput.container}>
+                                          
+                                            <ScheduleMap />
+                                          
+                                          
+                                        </form>
+                                      </DialogContent>
+                                      <DialogActions>
+                                        <Button onClick={this.handleClose} color="primary">
+                                          Cancel
+                                        </Button>
+                                        <Button onClick={this.handleClose} color="primary">
+                                          Ok
+                                        </Button>
+                                      </DialogActions>
+                                    </Dialog>
                                   </td>
                                 </tr>
                                 <tr>
